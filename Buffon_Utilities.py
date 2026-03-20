eec56568f2858b3e269b8a7b070dad24dfba232e
Hiba-N <hibanasir123@hotmail.com>
2026-03-20 00:30:41 -0400

    created function to estimate pi, using simulated value of probability

--- Buffon_Utilities.py
+++ Buffon_Utilities.py
@@ -1,24 +1,9 @@
-# 1) Create a file named Buffon_Utilities.py containing the following functions.
-
-# 1a) Write a function which takes two arguments, a
-#  and b
-# , randomly samples from the uniform distribution between a
-#  and b
-# , and returns the sampled value.
-
 import random
 import math
 
-#function to sample from a normal distribution given the range a - b
+
 def sample_uniform(a, b):
     return random.uniform(a, b) 
-
-
-
-# 1b) Write a function which, given x
-#  and θ
-#  and ℓ
-#  (the length of the needle), returns a boolean indicating whether or not the needle is touching the edge of the board.
 
 
 #function to check if needle touches line
@@ -36,14 +21,6 @@ def touches_line(mid_needle_distance, needle_angle, needle_length):
     else:
         return True
 
-
-
-# 1c) Write a function which, given ℓ (length of needle)
-# , t (length of wood)
-# , and n
-# , simulates the tossing of a needle n
-#  times. It should return the number of times, out of n
-# , that the needle touches the edge of the board.
 
 def needle_simulator(needle_length, wood_length, total_tosses):
     """
@@ -83,12 +60,7 @@ def touch_probability(needle_length, wood_length):
             + 1
         )
 
-# 1d) Write a function which takes ℓ
-#  and t
-#  as arguments. Using the formulae given in the link above, the function should return the theoretical probability
-#  of a single needle toss touching a board edge.
 
-# 1e) Write a function which takes a probability, ℓ
-#  and t
-#  as arguments. The function should return an estimate for the value of π
-# . This will require rearranging the equation used in part 1d) above. Be sure to not copy-and-paste code!
+#function to calculate pi based of simulated probability
+def pi_estimator(probability, needle_length, wood_length):
+    return ((2*needle_length)/(wood_length*probability))
